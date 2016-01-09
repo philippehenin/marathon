e92bb3faab36e7d0b4ccace005d61f13285998b2
Philippe Henin <philippe@henin.me>
2016-01-09 16:00:10 +0100

    add

---
+++ prereq.sh
@@ -0,0 +1,5 @@
+git clone https://git-wip-us.apache.org/repos/asf/mesos.git
+sudo apt-get install docker
+sudo apt-get install -y tar wget git
+sudo apt-get install -y openjdk-7-jdk
+sudo apt-get -y install build-essential python-dev python-boto libcurl4-nss-dev libsasl2-dev libsasl2-modules maven libapr1-dev libsvn-dev
