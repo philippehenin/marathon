33103e58d5166e2c00ecc2b0d01bd63c2f448cd7
Philippe Henin <philippe@henin.me>
2016-01-09 17:43:33 +0100

    correct?

--- build.sh
+++ build.sh
@@ -2,15 +2,28 @@ cd mesos
 ./bootstrap
 mkdir build
 ../configure
+
+
+
 make
+
+
 # si trop court en ram.... petite box cloud
 #free
 #dd if=/dev/zero of=/var/swap.img bs=1024k count=1000
 #mkswap /var/swap.img
 #swapon /var/swap.img
 #free
+
+
+# Pour les grosses configs en multi cores
 # make -j 4 V=0
+
+
+
 # make check to have the test cases
 make check
+
+
 #make install
 #or run from build
