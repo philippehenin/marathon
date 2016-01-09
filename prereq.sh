b174d54ddeb86da48e6d7743210a7feb10ebff87
philippehenin <philippehenin@gmail.com>
2016-01-09 16:50:51 +0100

    Update prereq.sh

--- prereq.sh
+++ prereq.sh
@@ -1,5 +1,7 @@
-git clone https://git-wip-us.apache.org/repos/asf/mesos.git
-sudo apt-get install docker
+
+sudo apt-get install -y docker
 sudo apt-get install -y tar wget git
 sudo apt-get install -y openjdk-7-jdk
 sudo apt-get -y install build-essential python-dev python-boto libcurl4-nss-dev libsasl2-dev libsasl2-modules maven libapr1-dev libsvn-dev
+
+git clone https://git-wip-us.apache.org/repos/asf/mesos.git
